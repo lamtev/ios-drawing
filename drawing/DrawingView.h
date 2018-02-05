0b9d05802d4415b44ad5eb15a12e269510733f36
lamtev <antonlamtev@gmail.com>
2018-02-05 12:07:35 +0300

    extract drawing to separate view

---
+++ drawing/DrawingView.h
@@ -0,0 +1,13 @@
+//
+//  DrawingView.h
+//  drawing
+//
+//  Created by anton.lamtev on 05.02.18.
+//  Copyright © 2018 anton.lamtev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawingView : UIView
+
+@end
---
+++ drawing/Line.m
@@ -0,0 +1,13 @@
+//
+//  Line.m
+//  drawing
+//
+//  Created by anton.lamtev on 05.02.18.
+//  Copyright © 2018 anton.lamtev. All rights reserved.
+//
+
+#import "Line.h"
+
+@implementation Line
+
+@end
