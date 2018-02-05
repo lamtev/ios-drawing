95a7cde6e69b929ea4f8a0f985eaa1c363e7bccf
lamtev <antonlamtev@gmail.com>
2018-02-05 12:07:35 +0300

    extract drawing to separate view

---
+++ drawing/DrawingView.m
@@ -0,0 +1,21 @@
+//
+//  DrawingView.m
+//  drawing
+//
+//  Created by anton.lamtev on 05.02.18.
+//  Copyright © 2018 anton.lamtev. All rights reserved.
+//
+
+#import "DrawingView.h"
+
+@implementation DrawingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ drawing/Line.h
@@ -0,0 +1,13 @@
+//
+//  Line.h
+//  drawing
+//
+//  Created by anton.lamtev on 05.02.18.
+//  Copyright © 2018 anton.lamtev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Line : NSObject
+
+@end
