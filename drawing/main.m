8fa888c8f1f249a6522c46ead048aa1cef5785d5
lamtev <antonlamtev@gmail.com>
2018-02-06 14:12:02 +0300

    add undo/redo

--- drawing/main.m
+++ drawing/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  drawing
-//
-//  Created by anton.lamtev on 30.01.18.
-//  Copyright © 2018 anton.lamtev. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
